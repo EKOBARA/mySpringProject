a88d1ca3f087bbc7e56c914b97d1a24ad5683503
ekobara <emmanuelkobara@gmail.com>
2025-09-17 20:38:38 -0400

    path variable

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.mySpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class HelloController {
         return "Hello World!";
     }
     @GetMapping("/{hero}")
-    public String heroPage(RequestParam String hero) {
+    public String heroPage(@PathVariable String hero) {
         return "This is page " + hero;
     }
 }
